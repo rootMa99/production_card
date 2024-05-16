cc5043d04d8bb1b35881ad08900b6ea53b8182e9
anass zeroual <anasszeroual09@gmail.com>
2024-05-16 16:52:48 +0100

    polab

--- src/components/production_card/Home.js
+++ src/components/production_card/Home.js
@@ -3,16 +3,28 @@ import c from "./Home.module.css";
 const Home = (p) => {
   return (
     <div className={c.container}>
-    <div className={c.title}>
+      <div className={c.title}>
         <span></span>
         <h3> head count statistics</h3>
         <span></span>
       </div>
       <div className={c.hcContainer}>
-        <div className={c.data}></div>
-        <div className={c.data}></div>
-        <div className={c.data}></div>
-        <div className={c.data}></div>
+        <div className={c.data}>
+          <h3>headcount</h3>
+          <span>78</span>
+        </div>
+        <div className={c.data}>
+          <h3>actual headcount</h3>
+          <span>78</span>
+        </div>
+        <div className={c.data}>
+          <h3>target</h3>
+          <span>78</span>
+        </div>
+        <div className={c.data}>
+          <h3>gap</h3>
+          <span>0</span>
+        </div>
       </div>
     </div>
   );
