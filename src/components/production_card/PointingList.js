5936c1f19edd7b65c3b04e0cb9acc7d8449efd3a
rootMa99 <anasszeroual09@gmail.com>
2024-05-19 14:51:01 +0100

    tyeazsqd

--- src/components/production_card/PointingList.js
+++ src/components/production_card/PointingList.js
@@ -1,16 +1,16 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import c from "./PointingList.module.css";
 
 const PointingList = (p) => {
   const [dataList, setDataList] = useState(p.data);
   const [inputValue, setInputValue] = useState("");
+  const [eid, setEid] = useState("");
   useEffect(() => {
     setDataList(p.data);
   }, [p.data]);
   // console.log(dataList, p.data);
   const changeHandler = (e) => {
     const value = e.target.value;
-    console.log(value, /^\d*$/.test(value), value.trim() !== "");
     if (/^\d*$/.test(value)) {
       setInputValue(value);
       if (e.target.value.trim() !== "") {
@@ -58,27 +58,30 @@ const PointingList = (p) => {
       <div className={c.wraper}>
         {dataList.length > 0 ? (
           dataList.map((m) => (
-            <div className={c.trainingH} key={m._id}>
-              <div className={c.trainingD}>
-                <div className={c.dataT} style={{ width: "33%" }}>
-                  <h3>{m.matricule}</h3>
+            <React.Fragment>
+              <div className={c.trainingH} key={m._id} onClick={e=>setEid(m._id)}>
+                <div className={c.trainingD}>
+                  <div className={c.dataT} style={{ width: "33%" }}>
+                    <h3>{m.matricule}</h3>
+                  </div>
+                  <div className={c.dataT} style={{ width: "33%" }}>
+                    <h3>{m.poste}</h3>
+                  </div>
+                  <div className={c.dataT} style={{ width: "33%" }}>
+                    <h3>shift</h3>
+                  </div>
                 </div>
-                <div className={c.dataT} style={{ width: "33%" }}>
-                  <h3>{m.poste}</h3>
-                </div>
-                <div className={c.dataT} style={{ width: "33%" }}>
-                  <h3>shift</h3>
+                <div className={c.trainingDi}>
+                  <div className={c.dataT} style={{ width: "50%" }}>
+                    <h3>7.67</h3>
+                  </div>
+                  <div className={c.dataT} style={{ width: "50%" }}>
+                    <h3>none</h3>
+                  </div>
                 </div>
               </div>
-              <div className={c.trainingDi}>
-                <div className={c.dataT} style={{ width: "50%" }}>
-                  <h3>7.67</h3>
-                </div>
-                <div className={c.dataT} style={{ width: "50%" }}>
-                  <h3>none</h3>
-                </div>
-              </div>
-            </div>
+              {eid === m._id && <div className={c.pointingEmpl}></div>}
+            </React.Fragment>
           ))
         ) : (
           <h4 className={c.noCrewS}>No employees were found</h4>
