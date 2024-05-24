bfdd5961ff2aed8e15c6807a699f32a5db80d404
anass zeroual <anasszeroual09@gmail.com>
2024-05-24 10:08:55 +0100

    fg

--- src/components/production_card/Pointing.js
+++ src/components/production_card/Pointing.js
@@ -1,5 +1,74 @@
 import React, { useState } from "react";
 import c from "./Pointing.module.css";
+import api from "../../service/api";
+import Select from "react-select";
+import DropdownIndicator from "../UI/DropdownIndicator";
+const customStyles = {
+  control: (provided, state) => ({
+    ...provided,
+    minWidth: "6rem",
+    minHeight: "10px",
+    fontSize: "8px",
+    textTransform: "uppercase",
+    borderRadius: "5px",
+    fontFamily: `Formular, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+                      "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+                      "Segoe UI Symbol"`,
+    textAlign: "center",
+    outline: "none",
+    border: "1px solid #F84018",
+    backgroundColor: "transparent",
+    boxShadow: "none",
+    margin: "auto",
+    "&:hover": {
+      border: "1px solid #f33716",
+      cursor: "pointer",
+    },
+  }),
+  option: (provided, state) => ({
+    width: "97%",
+    padding: "2px 0",
+    color: state.isFocused ? "#f3f3f3" : "#f33716",
+    backgroundColor: state.isFocused && "#474b4d",
+    fontFamily: `Formular, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+                      "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+                      "Segoe UI Symbol"`,
+    textTransform: "uppercase",
+    outline: "none",
+    textAlign: "center",
+    fontSize: "10px",
+    "&:hover": {
+      cursor: "pointer",
+    },
+  }),
+  input: (provided) => ({
+    ...provided,
+    color: "black",
+    fontSize: "10px",
+  }),
+  singleValue: (p) => ({
+    ...p,
+    color: "black",
+  }),
+  menuList: (provided) => ({
+    maxHeight: "70px",
+    overflowY: "auto",
+    overflowX: "hidden",
+    scrollbarWidth: "thin",
+    msOverflowStyle: "none",
+    "&::-webkit-scrollbar": {
+      width: "5px",
+      backgroundColor: "#535151",
+    },
+    "&::-webkit-scrollbar-thumb": {
+      backgroundColor: "#f33716",
+    },
+    "&::-webkit-scrollbar-track": {
+      backgroundColor: "transparent",
+    },
+  }),
+};
+
 
 const Pointing = (p) => {
   const [pc, setPc] = useState(false);
@@ -83,6 +152,30 @@ const Pointing = (p) => {
 
   return (
     <div className={c.container}>
+      {pc && (
+        <div className={c.pointingEmpl}>
+          <h3>{typet.title}</h3>
+          <div className={c.poinHoldWraper}>
+          <div className={c.poinHold}>
+              <span>Family</span>
+              <Select
+                components={{ DropdownIndicator }}
+                options={[]}
+                id="multiSelect"
+                inputId="shiftleader1"
+                styles={customStyles}
+                placeholder="select Family"
+              />
+            </div>
+          </div>
+          <div className={c.btnCn}>
+            <button className={c.submitShi} >
+              cancel
+            </button>
+            <button className={c.submitShi} style={{ color: "#f84018" }}>Submit</button>
+          </div>
+        </div>
+      )}
       {!pc && (
         <React.Fragment>
           <div
@@ -90,6 +183,18 @@ const Pointing = (p) => {
             onClick={(e) => onClickHandler(e, "ab")}
           >
             <div className={c.card}>AB</div>
+          </div>
+          <div
+            className={c.cardContainer}
+            onClick={(e) => onClickHandler(e, "ap")}
+          >
+            <div className={c.card}>AP</div>
+          </div>
+          <div
+            className={c.cardContainer}
+            onClick={(e) => onClickHandler(e, "shift")}
+          >
+            <div className={c.card}>shift</div>
           </div>
           <div
             className={c.cardContainer}
