457efd1a3b9f1363301b9c1307c37032369b9f8a
anass zeroual <anasszeroual09@gmail.com>
2024-05-30 10:33:21 +0100

    412

--- src/components/overTime/HomeOt.js
+++ src/components/overTime/HomeOt.js
@@ -3,6 +3,7 @@ import Select from "react-select";
 import CreatableSelect from "react-select/creatable";
 import DropdownIndicator from "../UI/DropdownIndicator";
 import { useEffect, useState } from "react";
+import { calculateHours } from "../hooks/daterelated";
 
 const customStyles = {
   control: (provided, state) => ({
@@ -71,34 +72,6 @@ const customStyles = {
   }),
 };
 
-const calculateHours = (startTimeStr, endTimeStr) => {
-    const isValidTimeFormat = (timeStr) => {
-      const timeParts = timeStr.split(":");
-      if (timeParts.length !== 2) return false;
-      const hours = parseInt(timeParts[0], 10);
-      const minutes = parseInt(timeParts[1], 10);
-      return !isNaN(hours) && !isNaN(minutes) && hours >= 0 && hours < 24 && minutes >= 0 && minutes < 60;
-    };
-    if (!isValidTimeFormat(startTimeStr) || !isValidTimeFormat(endTimeStr)) {
-      throw new Error("Invalid time format");
-    }
-    const startTimeParts = startTimeStr.split(":");
-    const startHours = parseInt(startTimeParts[0], 10);
-    const startMinutes = parseInt(startTimeParts[1], 10);
-    const endTimeParts = endTimeStr.split(":");
-    const endHours = parseInt(endTimeParts[0], 10);
-    const endMinutes = parseInt(endTimeParts[1], 10);
-    const startTime = new Date();
-    startTime.setHours(startHours, startMinutes, 0, 0); 
-    const endTime = new Date();
-    endTime.setHours(endHours, endMinutes, 0, 0); 
-    if (endTime < startTime) {
-      endTime.setDate(endTime.getDate() + 1);
-    }
-    const timeDifference = endTime.getTime() - startTime.getTime();
-    const hoursDifference = timeDifference / (1000 * 60 * 60);  
-    return hoursDifference;
-  };
 
 const HomeOt = () => {
   const [hours, setHours] = useState({
@@ -222,7 +195,7 @@ const HomeOt = () => {
           </label>
         </div>
         {ovf.includes(25) && (
-            <div className={c.pointingEmpl} style={{ marginBottom: "1rem" }}>
+            <div className={c.pointingEmpl}>
               <div className={c.poinHold}>
                 <span>Number of hours</span>
                 <input
@@ -253,7 +226,7 @@ const HomeOt = () => {
           </label>
         </div>
         {ovf.includes(50) && (
-            <div className={c.pointingEmpl} style={{ marginBottom: "1rem" }}>
+            <div className={c.pointingEmpl}>
               <div className={c.poinHold}>
                 <span>Number of hours</span>
                 <input
@@ -284,7 +257,7 @@ const HomeOt = () => {
           </label>
         </div>
         {ovf.includes(100) && (
-          <div className={c.pointingEmpl} style={{ marginBottom: "1rem" }}>
+          <div className={c.pointingEmpl}>
             <div className={c.poinHold}>
               <span>Number of hours</span>
               <input
