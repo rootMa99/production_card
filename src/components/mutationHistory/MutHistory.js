6a03638a5bdb897369027a6c79a43286ff6b110a
anass zeroual <anasszeroual09@gmail.com>
2024-06-06 08:58:33 +0100

    ert

--- src/components/mutationHistory/MutHistory.js
+++ src/components/mutationHistory/MutHistory.js
@@ -2,6 +2,12 @@ import c from "./MutHistory.module.css";
 
 const MutHistory=p=>{
 
+    return <div className={c.container}>
+    <div className={c.title2}>
+        <div className={c.line}></div>
+        <h4>Mutation history</h4>
+      </div>
+    </div>
 }
 
 export default MutHistory;
--- src/components/routes/TeamLeader.js
+++ src/components/routes/TeamLeader.js
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import { Navigate, Route, Routes } from "react-router";
 import Home from "../production_card/Home";
 import HomeOt from "../overTime/HomeOt";
+import MutHistory from "../mutationHistory/MutHistory";
 
 
 const TeamLeader = (p) => {
@@ -11,7 +12,7 @@ const TeamLeader = (p) => {
         <Route index path="/" element={<Navigate replace to="/cp" />} />
         <Route exact path="/cp" element={<Home />} />
         <Route exact path="/dhc" element={<HomeOt />} />
-        <Route exact path="/mth" element={<h1>muta</h1>} />
+        <Route exact path="/mth" element={<MutHistory />} />
         <Route path="*" element={<Navigate replace to="/cp" />} />
       </Routes>
     </Suspense>
