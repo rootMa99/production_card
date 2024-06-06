85c37618f270bf9dee0203bd0277b4613f0087d4
anass zeroual <anasszeroual09@gmail.com>
2024-06-06 09:26:35 +0100

    sd

--- src/components/mutationHistory/MutHistory.js
+++ src/components/mutationHistory/MutHistory.js
@@ -1,9 +1,10 @@
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import c from "./MutHistory.module.css";
 import api from "../../service/api";
 import { useSelector } from "react-redux";
 const MutHistory = (p) => {
   const { isLoged } = useSelector((s) => s.login);
+  const [data, setData]=useState([]);
   const callbackmu = useCallback(async () => {
     try {
       const response = await fetch(`${api}/employee/transfer`, {
@@ -18,6 +19,7 @@ const MutHistory = (p) => {
       }
       const d = await response.json();
       console.log("cl1m:", d);
+      setData(d)
     } catch (e) {
       console.error(e);
     }
@@ -33,8 +35,11 @@ const MutHistory = (p) => {
       </div>
       <div className={c.trainingH}>
         <div className={c.dater}>
-          <div className={c.dataT}>
+          <div className={c.dataT} style={{ width: "60%" }}>
             <h3>date</h3>
+          </div>
+          <div className={c.dataT} style={{ width: "40%" }}>
+            <h3>cmt by</h3>
           </div>
         </div>
         <div className={c.trainingDi}>
@@ -45,14 +50,16 @@ const MutHistory = (p) => {
             <h3>crew</h3>
           </div>
         </div>
-        <div className={c.trainingD} >
+        <div className={c.trainingD}>
           <div className={c.dataT} style={{ width: "50%" }}>
-            <h3>hours</h3>
+            <h3>teamleader</h3>
           </div>
           <div className={c.dataT} style={{ width: "50%" }}>
-            <h3>status</h3>
+            <h3>crew</h3>
           </div>
         </div>
+      </div>
+      <div className={c.wraper}>
       </div>
     </div>
   );
