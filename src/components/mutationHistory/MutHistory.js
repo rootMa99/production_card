0efccb75feff7ba82c2f9bae0a2b7bccf86f6322
anass zeroual <anasszeroual09@gmail.com>
2024-06-06 09:34:31 +0100

    ui

--- src/components/mutationHistory/MutHistory.js
+++ src/components/mutationHistory/MutHistory.js
@@ -4,7 +4,7 @@ import api from "../../service/api";
 import { useSelector } from "react-redux";
 const MutHistory = (p) => {
   const { isLoged } = useSelector((s) => s.login);
-  const [data, setData]=useState([]);
+  const [data, setData] = useState([]);
   const callbackmu = useCallback(async () => {
     try {
       const response = await fetch(`${api}/employee/transfer`, {
@@ -19,7 +19,7 @@ const MutHistory = (p) => {
       }
       const d = await response.json();
       console.log("cl1m:", d);
-      setData(d)
+      setData(d);
     } catch (e) {
       console.error(e);
     }
@@ -60,6 +60,38 @@ const MutHistory = (p) => {
         </div>
       </div>
       <div className={c.wraper}>
+        {data.length > 0 ? (
+          data.map((m) => (
+            <div className={c.trainingH} key={m._id} style={{ margin: 0 }}>
+              <div className={c.dater}>
+                <div className={c.dataT} style={{ width: "60%" }}>
+                  <h3>{m.date.split("T")[0]}</h3>
+                </div>
+                <div className={c.dataT} style={{ width: "40%" }}>
+                  <h3>{m.requestedBy}</h3>
+                </div>
+              </div>
+              <div className={c.trainingDi}>
+                <div className={c.dataT} style={{ width: "50%" }}>
+                  <h3>{m.from.teamleader}</h3>
+                </div>
+                <div className={c.dataT} style={{ width: "50%" }}>
+                  <h3>{m.from.crew}</h3>
+                </div>
+              </div>
+              <div className={c.trainingD}>
+                <div className={c.dataT} style={{ width: "50%" }}>
+                  <h3>{m.to.teamleader}</h3>
+                </div>
+                <div className={c.dataT} style={{ width: "50%" }}>
+                  <h3>{m.to.crew}</h3>
+                </div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <h3>no</h3>
+        )}
       </div>
     </div>
   );
