763d8c17e8d4e09f9a55048705cedceafa4e103f
anass zeroual <anasszeroual09@gmail.com>
2024-06-07 16:38:53 +0100

    qsd

--- src/components/moderator/Output.js
+++ src/components/moderator/Output.js
@@ -9,42 +9,24 @@ const Output = (p) => {
       </div>
       <div className={c.trainingH}>
         <div className={c.dater}>
-          <div className={c.dataT} style={{ width: "60%" }}>
+          <div className={c.dataT}>
             <h3 style={{ color: "#E5E1DA" }}>date</h3>
-          </div>
-          <div className={c.dataT} style={{ width: "40%" }}>
-            <h3 style={{ color: "#E5E1DA" }}>cmt by</h3>
           </div>
         </div>
         <div
           className={c.dater}
-          style={{ backgroundColor: "#383942", width: "20%" }}
+          style={{ backgroundColor: "#383942", width: "50%" }}
         >
           <div className={c.dataT}>
-            <h3>employee</h3>
+            <h3>crew</h3>
           </div>
         </div>
         <div className={c.trainingDi}>
           <div className={c.dataT} style={{ width: "50%" }}>
             <h3>tl src</h3>
           </div>
-          <div className={c.dataT} style={{ width: "50%" }}>
-            <h3>crew src</h3>
-          </div>
         </div>
-        <div className={c.trainingD}>
-          <div className={c.dataT} style={{ width: "50%" }}>
-            <h3>tl des</h3>
-          </div>
-          <div className={c.dataT} style={{ width: "50%" }}>
-            <h3>crew des</h3>
-          </div>
-        </div>
-        <div className={c.dater} style={{ width: "20%" }}>
-          <div className={c.dataT}>
-            <h3 style={{ color: "#FFA211" }}>mut type</h3>
-          </div>
-        </div>
+
       </div>
       <div className={c.wraper}></div>
     </div>
