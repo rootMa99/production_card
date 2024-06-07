8e1f98caf20864f64b12a000b874ed205305773a
anass zeroual <anasszeroual09@gmail.com>
2024-06-07 16:20:14 +0100

    sdf

---
+++ src/components/moderator/AbsTloOutput.js
@@ -0,0 +1,9 @@
+import c from "./AbsTloOutput.module.css";
+
+const AbsTloOutput = (p) => {
+
+    
+  return <div className={c.container}></div>;
+};
+
+export default AbsTloOutput;
---
+++ src/components/moderator/Output.js
@@ -0,0 +1,6 @@
+
+
+const output=p=>{
+
+}
+
