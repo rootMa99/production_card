7e79d2d5442aa08b59a46b909b2b7dcdd346b71a
anass zeroual <anasszeroual09@gmail.com>
2024-06-10 10:44:13 +0100

    erer

--- src/components/moderator/Output.js
+++ src/components/moderator/Output.js
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import c from "./Output.module.css";
 import React, { useCallback, useEffect, useState } from "react";
 import api from "../../service/api";
-
+import * as ExcelJS from "exceljs";
 const tq = (d) => {
   let t = 0;
   d.forEach((e) => {
@@ -47,12 +47,105 @@ const Output = (p) => {
     console.log(o[0]);
     setDataOutp(o[0]);
   };
+
+  const generateExcel = () => {
+    const workbook = new ExcelJS.Workbook();
+    const worksheet = workbook.addWorksheet("output");
+
+    if (p.data.length > 0) {
+      const columns = [
+        "matricule",
+        "prenom",
+        "nom",
+        "categorie",
+        "categorie de formation",
+        "formation",
+        "durée par heure",
+        "date de début",
+        "date de fin",
+        "préstataire",
+        "modalité",
+        "formatteur",
+        "eva",
+      ];
+      worksheet.columns = columns.map((column) => ({
+        header: column,
+        key: column,
+        width: column === "formation" ? 50 : 25,
+        height: 15,
+        filterButton: true,
+      }));
+      worksheet.eachRow({ includeEmpty: false }, function (row, rowNumber) {
+        if (rowNumber === 1) {
+          row.eachCell({ includeEmpty: true }, function (cell) {
+            cell.fill = {
+              type: "pattern",
+              pattern: "solid",
+              fgColor: { argb: "FFA211" },
+            };
+            cell.font = { bold: true };
+          });
+        }
+      });
+
+      p.data.forEach((obj, index) => {
+        const row = {
+          matricule: obj.es.matricule,
+          prenom: obj.es.prenom,
+          nom: obj.es.nom,
+          categorie: obj.es.category,
+          "categorie de formation": obj.trainingType,
+          formation: obj.trainingTitle,
+          "durée par heure": obj.dph.toFixed(2),
+          "date de début": obj.ddb,
+          "date de fin": obj.ddf,
+          préstataire: obj.prestataire,
+          modalité: obj.modalite,
+          formatteur: obj.formatteur,
+          eva: obj.eva,
+        };
+
+        const worksheetRow = worksheet.addRow(row);
+        if (index % 2 === 0) {
+          worksheetRow.eachCell({ includeEmpty: true }, function (cell) {
+            cell.fill = {
+              type: "pattern",
+              pattern: "solid",
+              fgColor: { argb: "D3D3D3" }, // Light gray color for even rows
+            };
+          });
+        }
+
+        worksheetRow.eachCell({ includeEmpty: true }, function (cell) {
+          cell.font = { color: { argb: "000000" } }; // Black font color
+        });
+        worksheet.eachRow((row, rowNumber) => {
+          row.eachCell((cell) => {
+            cell.alignment = { horizontal: "center", vertical: "center" };
+          });
+        });
+      });
+    }
+
+    workbook.xlsx.writeBuffer().then((buffer) => {
+      const blob = new Blob([buffer], {
+        type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "plan de formation.xlsx";
+      a.click();
+      URL.revokeObjectURL(url);
+    });
+  };
   return (
     <div className={c.container}>
       <div className={c.title2}>
         <div className={c.line}></div>
         <h4 style={{ fontSize: "22px" }}>output</h4>
       </div>
+      
       <div className={c.trainingH}>
         <div className={c.dater}>
           <div className={c.dataT}>
@@ -97,28 +190,55 @@ const Output = (p) => {
             </div>
             {oid === m._id && (
               <div className={c.plusData}>
-                {dataOutp.output.length>0?  <ul className={c.unList}>
-                  <li className={c.lis}>
-                    <span>family</span>
-                    <span>reference</span>
-                    <span>prod</span>
-                    <span>ce</span>
-                    <span>emb</span>
-                  </li>
-                  {
-                    dataOutp.output.map(m=><li className={c.lisb}>
+                {dataOutp.output.length > 0 ? (
+                  <ul className={c.unList}>
+                    <li className={c.lis}>
+                      <span>family</span>
+                      <span>reference</span>
+                      <span>prod</span>
+                      <span>ce</span>
+                      <span>emb</span>
+                    </li>
+                    {dataOutp.output.map((m) => (
+                      <li className={c.lisb}>
                         <span>{m.family}</span>
-                        <span style={{color:"#006B63", fontWeight:"800"}}> {m.reference}</span>
+                        <span style={{ color: "#006B63", fontWeight: "800" }}>
+                          {" "}
+                          {m.reference}
+                        </span>
                         <span>{m.prod}</span>
                         <span>{m.ce}</span>
-                        <span style={{color:"#CF3335", fontWeight:"800"}}>{m.emb}</span>
-                      </li>)
-                  }
-                </ul>: <h4 className={c.quatro}>no output found</h4>}
+                        <span style={{ color: "#CF3335", fontWeight: "800" }}>
+                          {m.emb}
+                        </span>
+                      </li>
+                    ))}
+                  </ul>
+                ) : (
+                  <h4 className={c.quatro}>NO OUTPUT HAS BEEN FOUND</h4>
+                )}
               </div>
             )}
           </React.Fragment>
         ))}
+      </div>
+      <div className={c.btnhelperholder}>
+        <button className={c.button} type="button" onClick={generateExcel}>
+          <span className={c["button__text"]}>Download</span>
+          <span className={c["button__icon"]}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 35 35"
+              id="bdd05811-e15d-428c-bb53-8661459f9307"
+              data-name="Layer 2"
+              className={c.svg}
+            >
+              <path d="M17.5,22.131a1.249,1.249,0,0,1-1.25-1.25V2.187a1.25,1.25,0,0,1,2.5,0V20.881A1.25,1.25,0,0,1,17.5,22.131Z"></path>
+              <path d="M17.5,22.693a3.189,3.189,0,0,1-2.262-.936L8.487,15.006a1.249,1.249,0,0,1,1.767-1.767l6.751,6.751a.7.7,0,0,0,.99,0l6.751-6.751a1.25,1.25,0,0,1,1.768,1.767l-6.752,6.751A3.191,3.191,0,0,1,17.5,22.693Z"></path>
+              <path d="M31.436,34.063H3.564A3.318,3.318,0,0,1,.25,30.749V22.011a1.25,1.25,0,0,1,2.5,0v8.738a.815.815,0,0,0,.814.814H31.436a.815.815,0,0,0,.814-.814V22.011a1.25,1.25,0,1,1,2.5,0v8.738A3.318,3.318,0,0,1,31.436,34.063Z"></path>
+            </svg>
+          </span>
+        </button>
       </div>
     </div>
   );
