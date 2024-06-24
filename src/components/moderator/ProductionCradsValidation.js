f83a7f425bf7d06bf5d41f1b5f2a242170a4b72f
anass zeroual <anasszeroual09@gmail.com>
2024-06-24 13:39:39 +0100

    yui

--- src/components/moderator/ProductionCradsValidation.js
+++ src/components/moderator/ProductionCradsValidation.js
@@ -1,10 +1,28 @@
+import { useState } from "react";
 import c from "./ProductionCradsValidation.module.css";
 
-
-const ProductionCradsValidation=p=>{
-
-    return <div className={c.container}></div>
-
-}
+const ProductionCradsValidation = (p) => {
+  const [date, setDate] = useState(new Date().toISOString().split("T")[0]);
+  return (
+    <div className={c.container}>
+      <div className={c.inputHolder}>
+        <div className={c.inputD}>
+          <h3>select date:</h3>
+          <input
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            max={new Date().toISOString().split("T")[0]}
+            pattern="yyyy-mm-dd"
+          />
+        </div>
+      </div>
+      <div className={c.title2}>
+        <div className={c.line}></div>
+        <h4 style={{ fontSize: "25px" }}>Production cards</h4>
+      </div>
+    </div>
+  );
+};
 
 export default ProductionCradsValidation;
