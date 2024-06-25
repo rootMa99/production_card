9944d8d608522797bbd2d5d41b9d5c79f81c9114
anass zeroual <anasszeroual09@gmail.com>
2024-06-25 11:11:53 +0100

    dsf

--- src/components/moderator/ProductionCardDetails.js
+++ src/components/moderator/ProductionCardDetails.js
@@ -1,21 +1,21 @@
 import c from "./ProductionCardDetails.module.css";
-import lg from "../../assets/aptiv-logo.svg"
+import lg from "../../assets/aptiv-logo.svg";
 const ProductionCardDetails = (p) => {
   return (
     <div className={c.container}>
       <div className={c.head}>
-        <h3>k01B</h3>
-        <h4>family/project</h4>
+        <h2>k01B</h2>
+        <h3>family/project</h3>
       </div>
       <div className={c.crewDetails}>
         <div className={c.details}>
-          <span>teamleader:</span> <span>some name</span>
+          <span>teamleader:</span> <span className={c.imp}>some name</span>
         </div>
         <div className={c.details}>
-          <span>shiftleader:</span> <span>some name</span>
+          <span>shiftleader:</span> <span className={c.imp}>some name</span>
         </div>
         <div className={c.details}>
-          <span>coordinator:</span> <span>some name</span>
+          <span>coordinator:</span> <span className={c.imp}>some name</span>
         </div>
       </div>
 
