50c6f6a64edb98949fc7c7e7158d2e5cd3ce8d65
anass zeroual <anasszeroual09@gmail.com>
2024-06-27 12:01:10 +0100

    qsd

--- src/components/moderator/ModifyPointing.js
+++ src/components/moderator/ModifyPointing.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import c from "./ModifyPointing.module.css";
 import Select from "react-select";
 import DropdownIndicator from "..//UI/DropdownIndicator";
 import CreatableSelect from "react-select/creatable";
+// import api from "../../service/api";
+// import { useSelector } from "react-redux";
 const customStyles = {
   control: (provided, state) => ({
     ...provided,
@@ -76,6 +78,7 @@ const customStyles = {
   }),
 };
 const ModifyPointing = (p) => {
+//   const { isLoged } = useSelector((s) => s.login);
   const o = p.data;
   const [saePoin, setpoin] = useState({
     status: o.status === undefined ? "" : o.status,
@@ -86,13 +89,49 @@ const ModifyPointing = (p) => {
     tDuration: o.t === undefined ? 0 : o.t,
     crDuration: o.cr === undefined ? 0 : o.cr,
     retardDuration: o.retard === undefined ? 0 : o.retard,
-    //todo: add ttl, and tcrew
-    ttl: "",
-    tCrew: "",
-    mutType: "",
+    // ttl: o.to === undefined ? "" : o.to.teamleader,
+    // tCrew: o.to === undefined ? "" : o.to.crew,
+    // mutType: o.to === undefined ? "" : o.to.isDefinitely,
     motif: o.motif === undefined ? "" : o.motif,
     details: o.details === undefined ? "" : o.details,
   });
+//   const [dataEp, setDataEp] = useState([]);
+//   const [tom, setTom] = useState([
+//     {
+//       id: Math.random(),
+//       teamleader: "",
+//       crew: "",
+//       paidHour: 0,
+//     },
+//   ]);
+//   const callback = useCallback(async () => {
+//     try {
+//       const response = await fetch(`${api}/employee/crews-by-tl/`, {
+//         method: "GET",
+//         headers: {
+//           "Content-Type": "application/json",
+//           Authorization: `Bearer ${isLoged.token}`,
+//         },
+//       });
+//       if (!response.ok) {
+//         throw new Error(response.status);
+//       }
+//       const d = await response.json();
+//       console.log("tl&crew:", d);
+//       setDataEp(d);
+//     } catch (e) {
+//       console.error(e);
+//     }
+//   }, [isLoged.token]);
+//   useEffect(() => {
+//     callback();
+//   }, [callback]);
+//   const changetlm = (i, v) => {
+//     const ind = tom.findIndex((f) => f.id === i);
+//     console.log(ind);
+//     return ind;
+//   };
+
   console.log(p.data, saePoin);
   return (
     <React.Fragment>
@@ -120,6 +159,49 @@ const ModifyPointing = (p) => {
         </div>
         <div className={c.pointingData}>
           <div className={c.poinHold}>
+            <span>Pointing</span>
+            <Select
+              components={{ DropdownIndicator }}
+              options={[
+                { label: "shift", value: "shift" },
+                { label: "admin", value: "admin" },
+                { label: "ab", value: "ab" },
+                { label: "ap", value: "ap" },
+                { label: "ma", value: "ma" },
+                { label: "mt", value: "mt" },
+                { label: "tl", value: "tl" },
+                { label: "t", value: "t" },
+                { label: "ctp", value: "ctp" },
+                { label: "di", value: "di" },
+                { label: "dn", value: "dn" },
+                { label: "te", value: "te" },
+                { label: "at", value: "at" },
+                { label: "cr", value: "cr" },
+                { label: "mutation", value: "mutation" },
+              ]}
+              id="multiSelect"
+              inputId="shiftleader1"
+              styles={customStyles}
+              placeholder="select shift"
+              onChange={(e) =>
+                setpoin((p) => ({
+                  ...p,
+                  pointingOptions: [],
+                  ctnDuration: 0,
+                  otDuration: 0,
+                  tDuration: 0,
+                  retardDuration: 0,
+                  crDuration: 0,
+                  pointing: e.value,
+                }))
+              }
+              value={{
+                label: saePoin.pointing,
+                value: saePoin.pointing,
+              }}
+            />
+          </div>
+          <div className={c.poinHold}>
             <span>motif</span>
             <Select
               components={{ DropdownIndicator }}
@@ -136,6 +218,10 @@ const ModifyPointing = (p) => {
               styles={customStyles}
               placeholder="select motif"
               onChange={(e) => setpoin((p) => ({ ...p, motif: e.value }))}
+              value={{
+                label: saePoin.motif,
+                value: saePoin.motif,
+              }}
             />
           </div>
           <div className={c.poinHold}>
@@ -160,9 +246,43 @@ const ModifyPointing = (p) => {
                   details: e === null ? "" : e.value,
                 }))
               }
+              value={{
+                label: saePoin.details,
+                value: saePoin.details,
+              }}
               placeholder="select details or enter"
             />
           </div>
+          <div className={c.poinHold}>
+            <span>Pointing exc</span>
+            <Select
+              components={{ DropdownIndicator }}
+              options={[
+                { label: "ot", value: "ot" },
+                { label: "ctn", value: "ctn" },
+                { label: "cr", value: "cr" },
+                { label: "t", value: "t" },
+                { label: "retard", value: "retard" },
+                { label: "mutation", value: "mutation" },
+              ]}
+              id="multiSelect"
+              inputId="shiftleader1"
+              styles={customStyles}
+              placeholder="select exc"
+              isMulti
+              value={saePoin.pointingOptions.map((m) => ({
+                label: m,
+                value: m,
+              }))}
+              onChange={(e) =>
+                setpoin((p) => ({
+                  ...p,
+                  pointingOptions: e.map((m) => m.value),
+                }))
+              }
+            />
+          </div>
+
           <div className={c.poinHold}>
             <span>cr duration</span>
             <input
@@ -240,3 +360,190 @@ const ModifyPointing = (p) => {
 };
 
 export default ModifyPointing;
+
+
+
+// {saePoin.pointing === "mutation" && (
+//     <React.Fragment>
+//       <div className={c.poinHold}>
+//         <span>targeted tl</span>
+//         <Select
+//           components={{ DropdownIndicator }}
+//           options={dataEp.map((m) => ({
+//             label: m.tl.fullname,
+//             value: m.tl.username,
+//           }))}
+//           id="multiSelect"
+//           inputId="shiftleader1"
+//           styles={customStyles}
+//           placeholder="select teamleader"
+//           onChange={(e) => setpoin((p) => ({ ...p, ttl: e.value }))}
+//           value={{
+//             label: saePoin.ttl,
+//             value: saePoin.ttl,
+//           }}
+//         />
+//       </div>
+//       {saePoin.ttl.trim() !== "" && (
+//         <div className={c.poinHold}>
+//           <span>targeted crew</span>
+//           <Select
+//             components={{ DropdownIndicator }}
+//             options={dataEp
+//               .filter((f) => f.tl.username === saePoin.ttl)[0]
+//               .crews.map((m) => ({
+//                 label: m,
+//                 value: m,
+//               }))}
+//             id="multiSelect"
+//             inputId="shiftleader1"
+//             styles={customStyles}
+//             placeholder="select crew"
+//             onChange={(e) =>
+//               setpoin((p) => ({
+//                 ...p,
+//                 tCrew: e.value,
+//               }))
+//             }
+//             value={{
+//                 label: saePoin.tCrew,
+//                 value: saePoin.tCrew,
+//               }}
+//           />
+//         </div>
+//       )}
+//       <div className={c.poinHold}>
+//         <span>mutation type</span>
+//         <Select
+//           components={{ DropdownIndicator }}
+//           options={[
+//             {
+//               label: "temporelle",
+//               value: "temporelle",
+//             },
+//             {
+//               label: "Définitivement",
+//               value: "Définitivement",
+//             },
+//           ]}
+//           id="multiSelect"
+//           inputId="shiftleader1"
+//           styles={customStyles}
+//           placeholder="select type"
+//           onChange={(e) => setpoin((p) => ({ ...p, mutType: e.value }))}
+//         />
+//       </div>
+//     </React.Fragment>
+//   )}
+//   {saePoin.pointingOptions.includes("mutation") && (
+//     <div className={c.mutexpx}>
+//       {tom.map((m) => (
+//         <div className={c.tomwrap} key={m.id}>
+//           <div className={c.poinHold}>
+//             <span>targeted tl</span>
+//             <Select
+//               components={{ DropdownIndicator }}
+//               options={dataEp.map((m) => ({
+//                 label: m.tl.fullname,
+//                 value: m.tl.username,
+//               }))}
+//               id="multiSelect"
+//               inputId="shiftleader1"
+//               styles={customStyles}
+//               placeholder="select teamleader"
+//               onChange={(e) => {
+//                 setTom((p) => {
+//                   return p.map((item, ind) => {
+//                     if (changetlm(m.id) === ind) {
+//                       return {
+//                         ...item,
+//                         teamleader: e.value,
+//                       };
+//                     }
+//                     return item;
+//                   });
+//                 });
+//               }}
+//               // value={{
+//               //   label: poin.ttl,
+//               //   value: poin.ttl,
+//               // }}
+//             />
+//           </div>
+//           {tom[changetlm(m.id)].teamleader.trim() !== "" && (
+//             <React.Fragment>
+//               <div className={c.poinHold}>
+//                 <span>targeted crew</span>
+//                 <Select
+//                   components={{ DropdownIndicator }}
+//                   options={dataEp
+//                     .filter(
+//                       (f) =>
+//                         f.tl.username ===
+//                         tom[changetlm(m.id)].teamleader
+//                     )[0]
+//                     .crews.map((m) => ({
+//                       label: m,
+//                       value: m,
+//                     }))}
+//                   id="multiSelect"
+//                   inputId="shiftleader1"
+//                   styles={customStyles}
+//                   placeholder="select crew"
+//                   onChange={(e) => {
+//                     setTom((p) => {
+//                       return p.map((item, ind) => {
+//                         if (changetlm(m.id) === ind) {
+//                           return {
+//                             ...item,
+//                             crew: e.value,
+//                           };
+//                         }
+//                         return item;
+//                       });
+//                     });
+//                   }}
+//                 />
+//               </div>
+//               <div className={c.poinHold}>
+//                 <span>duration</span>
+//                 <input
+//                   type="number"
+//                   placeholder="set duration"
+//                   onChange={(e) => {
+//                     setTom((p) => {
+//                       return p.map((item, ind) => {
+//                         if (changetlm(m.id) === ind) {
+//                           return {
+//                             ...item,
+//                             paidHour: +e.target.value / 60,
+//                           };
+//                         }
+//                         return item;
+//                       });
+//                     });
+//                   }}
+//                 />
+//               </div>
+//             </React.Fragment>
+//           )}
+//         </div>
+//       ))}
+//       <h5
+//         className={c.addnewinputs}
+//         onClick={(e) =>
+//           setTom((p) => [
+//             ...p,
+//             {
+//               id: Math.random(),
+//               teamleader: "",
+//               crew: "",
+//               paidHour: 0,
+//             },
+//           ])
+//         }
+//       >
+//         add new teamleader
+//       </h5>
+//     </div>
+//   )}
